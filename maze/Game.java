457de49a1b0ca530e602ff5c903d37c3afd91956
Alam <henri.alam@yahoo.com>
2018-04-25 17:19:46 +0200

    Added stopwatch feature

--- maze/Game.java
+++ maze/Game.java
@@ -1,6 +1,8 @@
+
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.lang.System.*;
 
 public class Game extends SimpleInterface{
     private Player player;
@@ -46,7 +48,7 @@ public class Game extends SimpleInterface{
 
     public void run(){
 	boolean exit = false;
-       
+	double start,end, time;
 	//Welcome screen
 	drawImage(loadImage("mazeWalker.jpg"), 0, 0, width, height);
 	refresh();
@@ -54,22 +56,28 @@ public class Game extends SimpleInterface{
 	world.fillScreen(this,player);
 	
 	//Start the game
+	start = System.nanoTime();
 	while(!exit){
-
+	    //System.out.println("debut = " + start);
 	    //Draw the result
 	    exit = !draw();
 	    // Check if the exit is near then display message box
 	    if(exitNear((int)(player.getX()), (int) (player.getY() ) ) ) {
-		printSuccess();
+		end = System.nanoTime();
+		//System.out.println("fin = " + end);
+		time = end - start;
+		//System.out.println("Vous avez mis "+ time/1000000000+"secondes nulos ...");
+		printSuccess(time/1000000000);
+		exit = true;
 	    }
 	}
 	closeWindow();
      }
 
-    public void printSuccess(){ // Message quand on vient de trouver la sortie 
+    public void printSuccess(double time){ // Message quand on vient de trouver la sortie 
 	JFrame frame = new JFrame("GAME OVER");
 	JOptionPane d = new JOptionPane();
-	d.showMessageDialog(frame, "Bien joué, vous avez gagné ! \n", "GAME OVER", JOptionPane.INFORMATION_MESSAGE);
+	d.showMessageDialog(frame, "Bien joué, vous avez gagné en " +time+" secondes ! \n", "GAME OVER", JOptionPane.INFORMATION_MESSAGE);
     }
 
     // returns true if the exit is within a range of one cell radius
